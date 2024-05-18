727d7ab3e03dc9a949c7af95b55dd83e45ce578d
Ross <ross.romero.2@gmail.com>
2024-05-18 19:34:38 -0400

    Refactor - Modularize Classes inside the main executable

--- src/DwarfRandomizer.py
+++ src/DwarfRandomizer.py
@@ -1,6 +1,12 @@
 import random 
-from enum import Enum
 from pathlib import Path
+from obj.Dwarf import Dwarf
+from obj.Skill import Skill
+from enums.OtherSkill import OtherSkill
+from enums.CombatSkill import CombatSkill
+from enums.LaborSkill import LaborSkill
+from enums.CrucialSkill import CrucialSkill
+
 
 def generateDwarves():
     dwarves = []
@@ -65,158 +71,6 @@ def generateSkill(skills):
             return skillFound
             
     return 
-    
-            
-            
-
-class OtherSkill(Enum):
-    SWIMMING = 1
-    DRESS_WOUNDS = 2
-    DIAGNOSE = 3
-    SURGERY = 4
-    SET_BONE = 5
-    SUTURE = 6
-    CRUTCH_WALK = 7
-    PERSUASION = 8
-    NEGOTIATION = 9
-    LYING = 10
-    INTIMIDATION = 11
-    JUDGING_INTENT = 12
-    APPRAISAL = 13
-    ORGANIZATION = 14
-    RECORD_KEEPING = 15
-    CONVERSATION = 16
-    COMEDY = 17
-    FLATTERY = 18
-    CONSOLE = 19
-    PACIFY = 20
-    INTRIGUE = 21
-    KNOWLEDGE_ACQUISITION = 22
-    CONCENTRATION = 23
-    WRITING = 24
-    PROSE = 25
-    POETRY = 26
-    READING = 27
-    SPEAKING = 28
-    PLAY_KEYBOARD_INSTRUMENT = 29
-    PLAY_STRINGED_INSTRUMENT = 30
-    PLAY_WIND_INSTRUMENT = 31
-    PLAY_PERCUSSION_INSRTUMENT = 32
-    DANCER = 33
-    CRITICAL_THINKING = 34
-    LOGIC = 35
-    MATHEMATICS = 36
-    ASTRONOMY = 37
-    CHEMISTRY = 38
-    GEOGRAPHY = 39
-    OPTICS_ENGINEER = 40
-    FLUID_ENGINEER = 41
-    LEADERSHIP = 42
-    MILITARY_TACTICS = 43
-    TEACHING = 44
-    SING = 45
-    MAKE_MUSIC = 46
-
-class CombatSkill(Enum):
-    AXE = 1
-    SWORD = 2
-    MACE = 3
-    HAMMER = 4
-    SPEAR = 5
-    CROSSBOW = 6
-    SHIELD = 7
-    ARMOR = 8
-    MELEE_COMBAT = 9
-    RANGED_COMBAT = 10
-    WRESTLING = 11
-    GRASP_STRIKE = 12
-    STANCE_STRIKE = 13
-    BITE = 14
-    DODGING = 15
-    SIEGECRAFT = 16
-    SIEGEOPERATE = 17
-    MISC_WEAPON = 18
-    DISCIPLINE = 19
-    SITUATIONAL_AWARENESS = 20
-
-class LaborSkill(Enum):
-    MECHANICS = 1
-    POTTERY = 2
-    GLAZING = 3
-    PAPERMAKING = 4
-    BOOKBINDING = 5
-    WAX_WORKING = 6
-    MILLING = 7
-    PROCESSPLANTS = 8
-    HERBALISM = 9
-    COOK = 10
-    WEAVING = 11
-    CLOTHESMAKING = 12
-    DYER = 13
-    TRAPPING = 14
-    SNEAK = 15
-    TANNER = 16
-    LEATHERWORK = 17
-    DISSECT_FISH = 18
-    DISSECT_VERMIN = 19
-    PROCESSFISH = 20
-    CHEESEMAKING = 21
-    MILK = 22
-    GELD = 23
-    SHEARING = 24
-    SPINNING = 25
-    PRESSING = 26
-    BEEKEEPING = 27
-    ANIMALTRAIN = 28
-    ANIMALCARE = 29
-    SOAP_MAKING = 30
-    LYE_MAKING = 31
-    POTASH_MAKING = 32
-    GLASSMAKER = 33
-    OPERATE_PUMP = 34
-
-class CrucialSkill(Enum):
-    MINING = 1
-    WOODCUTTING = 2
-    CARPENTRY = 3
-    MASONRY = 4
-    FORGE_WEAPON = 5
-    BOWYER = 6
-    FORGE_ARMOR = 7
-    FORGE_FURNITURE = 8
-    SMELT = 9
-    WOOD_BURNING = 10
-    FISH = 11
-    BUTCHER = 12
-    PLANT = 13
-    BREWING = 14
-    CUTGEM = 15
-    ENCRUSTGEM = 16
-    CUT_STONE = 17
-    CARVE_STONE = 18
-    ENGRAVE_STONE = 19
-    METALCRAFT = 20
-    STONECRAFT = 21
-    WOODCRAFT = 22
-    BONECARVE = 23
-
-
-class Dwarf:
-    def __init__(self, skills):
-        self.skills = skills
-
-    def getSkillsOutput(self,number):
-        ret = ""
-        for skill in self.skills:
-            ret += "\t[SKILL:" + str(number) + ":" + skill.skillname + ":" + str(skill.proficiency) + "]\n"
-        return ret;
-            
-    
-class Skill:
-    def __init__(self, name, proficiency):
-        self.skillname = name
-        self.proficiency = proficiency
-    
     
     
 def getFile():
